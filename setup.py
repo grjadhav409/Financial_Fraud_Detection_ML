a49150bde2b125e9f5d20fb343056ef0d019bcb2
ganesh jadhav <grjadhav409@gmail.com>
2023-03-19 19:42:19 +0530

    architecture setup

--- setup.py
+++ setup.py
@@ -2,6 +2,18 @@ from setuptools import find_packages,setup
 
 from typing import List
 
+def get_requirements()->List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirement_list:List[str] = []
+
+    """
+    Write a code to read requirements.txt file and append each requirements in requirement_list variable.
+    """
+    return requirement_list
+
+
 setup(
     name= "fraud_detection",
     version= "0.0.1",
@@ -9,11 +21,7 @@ setup(
     author_email= "grjadhav409@gmail.com",
     description= "A package to detect fraudulent transactions",
     packages=find_packages(),
-    # install_requires=[
-    #     "pandas",
-    #     "numpy",
-    #     "scikit-learn",
-    #     "matplotlib",]      # requirements.txt is already created
+    install_requires=get_requirements(),   
 )
 
 
