48f7a36ebad61463c78b17cc91a8d75c8f4f4aca
ganesh jadhav <grjadhav409@gmail.com>
2023-03-20 16:33:15 +0530

    schema added, data injection config added

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+from sensor.logger import logging
+import sys
+
+# for testing exceptions
+def test_exceptions():
+    try:
+        logging.info("Test exception of division")
+        x= 1/0
+    except Exception as e:
+        
+        raise SensorException(e,sys)
 
 
 if __name__ == '__main__':
-    mongo_client = MongoDBClient()
+    # # test-1:
+    # mongo_client = MongoDBClient()
 
-    print(mongo_client.database.list_collection_names())
+    # print(mongo_client.database.list_collection_names())
+
+    # # test-2:
+    try:
+        test_exceptions()
+
+    except Exception as e:
+        logging.error(e)
+        raise Exception
---
+++ sensor/entity/config_entity.py
@@ -0,0 +1,12 @@
+
+from datetime import datetime
+import os
+from sensor.constant  import training_pipeline
+
+class TrainingPipelineConfig:
+
+    def __init__(self,timestamp=datetime.now()):
+        timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
+        self.pipeline_name: str = training_pipeline.PIPELINE_NAME
+        self.artifact_dir: str = os.path.join(training_pipeline.ARTIFACT_DIR, timestamp)
+        self.timestamp: str = timestamp
